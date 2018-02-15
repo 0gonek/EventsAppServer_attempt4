4d593c4045829fe007c595a9af4e88dc0f013283
Ogonek <mihail.zatepyakin@gmail.com>
2018-02-15 14:41:44 +0300

    0.2.2 CORS filter

--- src/main/java/com/ogonek/eventsappserver/EventsAppServerApplication.java
+++ src/main/java/com/ogonek/eventsappserver/EventsAppServerApplication.java
@@ -1,12 +1,25 @@
 package com.ogonek.eventsappserver;
 
+import com.ogonek.eventsappserver.filter.CORSFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EventsAppServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventsAppServerApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistration() {
+		FilterRegistrationBean registrationBean =
+				new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS Filter");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return registrationBean;
 	}
 }
--- src/main/java/com/ogonek/eventsappserver/controller/TestController.java
+++ src/main/java/com/ogonek/eventsappserver/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ogonek.eventsappserver.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
